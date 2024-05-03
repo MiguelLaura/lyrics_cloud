318b0770b77db10f567a7d00d8edaaea6ca7bf34
Laura Miguel <miguellaura240@gmail.com>
2024-05-03 22:48:57 +0200

    correcting pre-commit

--- script/generate_readme.py
+++ script/generate_readme.py
@@ -3,9 +3,20 @@
 # =============================================================================
 #
 
+from contextlib import redirect_stdout
+import io
+
 from docdocdoc.build import generate_readme
 from lyrics_cloud.artists_lyrics import get_lyrics
 
 DOCS = [{"title": "Functions", "fns": [get_lyrics]}]
 
-generate_readme(DOCS)
+f = io.StringIO()
+
+with redirect_stdout(f):
+    generate_readme(DOCS)
+
+readme = f.getvalue()
+
+with open("README.md", "w") as file:
+    file.write(readme)
